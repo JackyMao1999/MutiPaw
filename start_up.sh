535b10706179e337d1a92bf1ddb8b48a4e860a2e
JackyMao1999 <837002144@qq.com>
2026-03-12 13:06:25 +0000

    opt code

--- start_up.sh
+++ start_up.sh
@@ -1,16 +1,18 @@
 # /bin/bash
 # 这是一个启动程序
-
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 # 1. 初始化环境
-if ! command -v python3-dev &>/dev/null; then
+if ! dpkg -s python3-dev &>/dev/null; then
     sudo apt install python3-dev
 fi
 
-if ! command -v python3-venv &>/dev/null; then
+if ! dpkg -s python3-venv &>/dev/null; then
     sudo apt install python3-venv
 fi
 # 2. 如果没有虚拟环境，创建一个虚拟环境
-python3 -m venv .venv
+if [[ ! -d ${SCRIPT_DIR}/.venv ]]; then
+    python3 -m venv .venv
+fi
 
 # 3. 进入虚拟环境之后安装requirements
 source .venv/bin/activate
